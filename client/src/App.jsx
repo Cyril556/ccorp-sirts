af6a3be76cdf7906ea718f33166d88cb2875e64c
Cyril556 <64355640+Cyril556@users.noreply.github.com>
2026-03-24 22:18:45 +0300

    feat: integrate Navbar into App.jsx layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext.jsx';
+import Navbar from './components/Navbar.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
 import IncidentsPage from './pages/IncidentsPage.jsx';
@@ -16,14 +17,17 @@ const PrivateRoute = ({ children, roles }) => {
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
-      <Route path="/incidents" element={<PrivateRoute><IncidentsPage /></PrivateRoute>} />
-      <Route path="/incidents/new" element={<PrivateRoute><NewIncidentPage /></PrivateRoute>} />
-      <Route path="/incidents/:id" element={<PrivateRoute><IncidentDetailPage /></PrivateRoute>} />
-      <Route path="/users" element={<PrivateRoute roles={['ADMIN']}><UsersPage /></PrivateRoute>} />
-      <Route path="*" element={<Navigate to="/dashboard" replace />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+        <Route path="/incidents" element={<PrivateRoute><IncidentsPage /></PrivateRoute>} />
+        <Route path="/incidents/new" element={<PrivateRoute><NewIncidentPage /></PrivateRoute>} />
+        <Route path="/incidents/:id" element={<PrivateRoute><IncidentDetailPage /></PrivateRoute>} />
+        <Route path="/users" element={<PrivateRoute roles={['ADMIN']}><UsersPage /></PrivateRoute>} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </>
   );
 }
